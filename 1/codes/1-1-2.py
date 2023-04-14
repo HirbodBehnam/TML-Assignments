9deb3568f0329fd7bf461ea37315a2a66a93f05e
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-04-14 23:44:02 +0430

    1-1-2

--- 1/codes/1-1-2.py
+++ 1/codes/1-1-2.py
@@ -9,7 +9,7 @@ def char_ok(a: str, char: str) -> bool:
     second_index = nth_index(a, char, 1)
     if first_index == None or second_index == None:
         return False
-    return second_index - first_index <= 1
+    return second_index - first_index <= 2
 
 def ok(a: str) -> bool:
     if a.count("a") < 2 and a.count("c") < 2:
@@ -17,6 +17,8 @@ def ok(a: str) -> bool:
     if char_ok(a, "a") or char_ok(a, "c"):
         return True
     return False
+
+print(ok("caca"))
 
 alphabet = ["a","b","c"]
 ok_states = []
@@ -27,5 +29,16 @@ for a in alphabet:
                 if ok(a + b + c + d):
                     ok_states.append(a + b + c + d)
 
+def state_name(state: str) -> str:
+    return "$q_{" + state + "}$"
+
 print(ok_states)
 print(len(ok_states))
+for state in ok_states:
+    next_a = state[1:] + "a"
+    next_b = state[1:] + "b"
+    next_c = state[1:] + "c"
+    next_a = state_name(next_a) if next_a in ok_states else ""
+    next_b = state_name(next_b) if next_b in ok_states else ""
+    next_c = state_name(next_c) if next_c in ok_states else ""
+    print(f"{state_name(state)} & {next_a} & {next_b} & {next_c} \\\\")
